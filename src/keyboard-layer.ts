4e4bdea01afe7eba995d958148b7b79f78f77442
Riley Mathews <rileymathews80@gmail.com>
2019-04-21 15:11:06 -0500

    added basic way to eat all inputs when layer is on

--- src/keyboard-layer.ts
+++ src/keyboard-layer.ts
@@ -2,14 +2,17 @@ import * as vscode from 'vscode'
 import StatusDisplay from './status-display'
 
 export default class KeyboardLayer {
+  private static readonly emptyDisposable = vscode.Disposable.from({ dispose: () => { } });
   public isActive: boolean;
   public configuration: vscode.WorkspaceConfiguration;
   private statusDisplay: StatusDisplay;
+  private typeDisposable: vscode.Disposable;
 
   public constructor (statusDisplay: StatusDisplay) {
     this.isActive = false;
     this.configuration = vscode.workspace.getConfiguration('keyboardlayer');
     this.statusDisplay = statusDisplay;
+    this.typeDisposable = KeyboardLayer.emptyDisposable;
   }
 
   public enable() {
@@ -27,6 +30,17 @@ export default class KeyboardLayer {
   private setActive(bool: boolean) {
     const config = vscode.workspace.getConfiguration('keyboardlayer');
     config.update('active', bool, true);
+    this.registerTypeCommand(bool)
     bool ? this.statusDisplay.displayEnabled() : this.statusDisplay.displayDisabled();
+  }
+
+  private registerTypeCommand(bool: boolean) {
+    if (bool) {
+      this.typeDisposable = vscode.commands.registerCommand('type', async _args => {
+        return;
+      })
+    } else {
+      this.typeDisposable.dispose()
+    }
   }
 }
