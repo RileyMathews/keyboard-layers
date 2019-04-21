6d68788d8f447cd7b72bd4ad43a92b6e80546a85
Riley Mathews <rileymathews80@gmail.com>
2019-04-21 15:15:18 -0500

    removed code to eat all inputs

--- src/keyboard-layer.ts
+++ src/keyboard-layer.ts
@@ -2,17 +2,14 @@ import * as vscode from 'vscode'
 import StatusDisplay from './status-display'
 
 export default class KeyboardLayer {
-  private static readonly emptyDisposable = vscode.Disposable.from({ dispose: () => { } });
   public isActive: boolean;
   public configuration: vscode.WorkspaceConfiguration;
   private statusDisplay: StatusDisplay;
-  private typeDisposable: vscode.Disposable;
 
   public constructor (statusDisplay: StatusDisplay) {
     this.isActive = false;
     this.configuration = vscode.workspace.getConfiguration('keyboardlayer');
     this.statusDisplay = statusDisplay;
-    this.typeDisposable = KeyboardLayer.emptyDisposable;
   }
 
   public enable() {
@@ -30,17 +27,6 @@ export default class KeyboardLayer {
   private setActive(bool: boolean) {
     const config = vscode.workspace.getConfiguration('keyboardlayer');
     config.update('active', bool, true);
-    this.registerTypeCommand(bool)
     bool ? this.statusDisplay.displayEnabled() : this.statusDisplay.displayDisabled();
-  }
-
-  private registerTypeCommand(bool: boolean) {
-    if (bool) {
-      this.typeDisposable = vscode.commands.registerCommand('type', async _args => {
-        return;
-      })
-    } else {
-      this.typeDisposable.dispose()
-    }
   }
 }
