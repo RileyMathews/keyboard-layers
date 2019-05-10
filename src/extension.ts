4ab21c11862ba8689be8605b9832ffba3617ced7
Riley Mathews <riley.mathews@daveramsey.com>
2019-05-10 11:17:58 -0500

    [new-configuration-options] user can now configure the notification type they would like

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,25 @@
 import * as vscode from 'vscode';
 import KeyboardLayer from './keyboard-layer';
 import ColorChangeStatusDisplay from './status-display-interface/color-change-status-display';
-import StatusBarStatusDisplay from './status-display-interface/status-bar-status-display'
+import StatusBarStatusDisplay from './status-display-interface/status-bar-status-display';
 
 
 export function activate(context: vscode.ExtensionContext) {
+	const config :String = vscode.workspace.getConfiguration('keyboardlayer').get('notification', 'Status Bar Icon');
 
-	const app = new KeyboardLayer(new ColorChangeStatusDisplay);
+	let notification :StatusDisplayInterface;
+	switch(config) {
+		case 'Status Bar Icon':
+			notification = new StatusBarStatusDisplay;
+			break;
+		case 'Theme Color Change':
+			notification = new ColorChangeStatusDisplay;
+			break;
+		default:
+			notification = new StatusBarStatusDisplay;
+	}
+
+	const app = new KeyboardLayer(notification);
 
 	let enableLayer = vscode.commands.registerCommand('keyboardlayer.activate', () => {
 		app.enable();
